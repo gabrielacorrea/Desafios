ccbd584d7fd79531be073e0d12cd98c164ac2b5a
gabrielacorrea <gabriela01sc@gmail.com>
2014-02-23 20:44:36 -0300

    Conclusao do desafio em java

---
+++ src/quebraLinha/Escritor.java
@@ -0,0 +1,39 @@
+package quebraLinha;
+
+public class Escritor {
+
+	private VoQuebraLinha pojo = new VoQuebraLinha();
+	private String fraseQuebrada = "";
+	private int espaco = 0;
+
+	public Escritor(VoQuebraLinha vo) {
+		pojo = vo;
+	}
+
+	public void quebraFrase() {
+		String[] vPalavras = pojo.getFrase().split(" ");
+		this.espaco = pojo.getQtdColunas();
+
+		for (int i = 0; i < vPalavras.length; i++) {
+			this.fraseQuebrada += this.quebraColunasDaFrase(vPalavras[i]);
+		}
+	}
+
+	public String quebraColunasDaFrase(String palavra) {
+		String palavraQuebrada = "";
+
+		if (palavra.length() < this.espaco) {
+			this.espaco -= palavra.length();
+			palavraQuebrada = palavra + " ";
+		} else {
+			this.espaco = pojo.getQtdColunas() - palavra.length();
+			palavraQuebrada = "\n" + palavra + " ";
+		}
+		return palavraQuebrada;
+	}
+
+	public void escreveFrase() {
+		System.out.println("Resultado: \n" + this.fraseQuebrada);
+	}
+
+}
